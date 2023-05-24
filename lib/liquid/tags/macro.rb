78ab4e8c00adf3af6ffc95593a954a89482bfbbe
davideandreoli-lmit <davide.andreoli@leroymerlin.it>
2023-05-24 21:27:13 +0200

    Improved macro parsing

--- lib/liquid/tags/macro.rb
+++ lib/liquid/tags/macro.rb
@@ -19,12 +19,99 @@ class Macros
         end
     end
 end
-       
 
+class MacroParser
+    def initialize(string_to_parse)
+        @string_to_parse = string_to_parse.chars
+        @reading_position = 0
+    end
+
+    def next
+        current_reading_position = @reading_position
+        @reading_position = @reading_position + 1
+        @string_to_parse[current_reading_position]
+    end
+
+    def peek
+        @string_to_parse[@reading_position]
+    end
+
+    def end_of_file
+        @reading_position == @string_to_parse.count
+    end
+
+    def is_alphanumeric()
+        self.peek.match?(/\w/)
+    end
+
+    def is_whitespace()
+        self.peek.match?(/\s/)
+    end
+
+    def is_quotes()
+        self.peek.match?(/["']/)
+    end
+
+    def is_parenthesis()
+        self.peek.match?(/[()]/)
+    end
+
+    def is_assignment()
+        self.peek.match?(/=/)
+    end
+
+    def read_string
+        starting_quote = self.next
+        token = ""
+        while ! self.end_of_file do
+            if self.is_quotes == false
+                token = token + self.next
+            else
+                if self.peek == starting_quote
+                    break
+                else
+                    token = token + self.next
+                end
+            end
+        end
+        token
+    end
+
+    def read_token(skip_spaces = true)
+        token = ""
+        while ! self.end_of_file do
+            if self.is_alphanumeric == true
+                token = token + self.next
+            elsif self.is_whitespace && skip_spaces == true
+                break
+            elsif self.is_whitespace && skip_spaces == false
+                token = token + self.next
+            else
+                break
+            end
+        end
+        token
+    end
+
+    def parse
+        list = []
+        while ! self.end_of_file do
+            if self.is_alphanumeric == true
+                list.append(self.read_token)
+            elsif self.is_quotes == true
+                list.append(self.read_string)
+            else
+                self.next
+            end
+        end
+        list
+    end
+end
+  
 class Macro < Liquid::Block
     def initialize(tag_name, markup, tokens)
        super
-       @macro_tokens = markup.gsub(/[,()]/, ' ').gsub(/\s+/, ' ').split(' ')
+       @macro_tokens = MacroParser.new(markup)
        @macro_name = @macro_tokens.shift.to_sym
        @macro_arguments = @macro_tokens
     end
