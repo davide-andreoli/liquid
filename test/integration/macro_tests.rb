1e79d1f4612f92f3f3ac34dc23a25e0244830aa1
Davide Andreoli <davide.andreoli@icloud.com>
2023-06-11 23:56:44 +0200

    Added test

--- test/integration/macro_tests.rb
+++ test/integration/macro_tests.rb
@@ -33,4 +33,11 @@ class MacroBlockUnitTest < Minitest::Test
     assert_equal("Hello World. Your name is Davide Andreoli and you are 5 years old.", macro_call.render)
   end
 
+  def test_macro_assignment_with_default_argument
+    macro_definition = Liquid::Template.parse('{% macro hello(name, age=18) %}Hello World. Your name is {$ name $} and you are {$ age $} years old.{% endmacro %}')
+    macro_definition.render
+    macro_call = Liquid::Template.parse('{% call_macro hello Davide 5 %}')
+    assert_equal("Hello World. Your name is Davide and you are 5 years old.", macro_call.render)
+  end
+
 end
