584a9751c46ea98ea1e767c05919c1d0fbeb863b
davideandreoli-lmit <davide.andreoli@leroymerlin.it>
2023-07-27 11:53:24 +0200

    Added comments

--- lib/liquid/tags/macro.rb
+++ lib/liquid/tags/macro.rb
@@ -72,52 +72,89 @@ class Macros
 end
 
 class MacroParser
+    """
+        This class is used to parse macros.
+    """
     def initialize(string_to_parse)
+        """
+            The class is initialized by stripping whitespaces from the string and setting the reading position to 0.
+        """
         @string_to_parse = string_to_parse.rstrip.lstrip.chars
         @reading_position = 0
     end
 
     def next
+        """
+            This function return the current character and advance the reading position.
+        """
         current_reading_position = @reading_position
         @reading_position = @reading_position + 1
         @string_to_parse[current_reading_position]
     end
 
     def peek
+        """
+            This function return the current character.
+        """
         @string_to_parse[@reading_position]
     end
 
     def end_of_file
+        """
+            This function returns true if the end of file is reached.
+        """
         @reading_position == @string_to_parse.count
     end
 
     def is_alphanumeric()
+        """
+            This function returns true if the current character is an alphanumeric.
+        """
         self.peek.match?(/\w/)
     end
 
     def is_whitespace()
+        """
+            This function returns true if the current character is a whitespace.
+        """
         self.peek.match?(/\s/)
     end
 
     def is_quotes()
+        """
+            This function returns true if the current character is a quote.
+        """
         self.peek.match?(/["']/)
     end
 
     def is_parenthesis()
+        """
+            This function returns true if the current character is a round parenthesis.
+        """
         self.peek.match?(/[()]/)
     end
 
     def is_assignment()
+        """
+            This function returns true if the current character is an equal sign.
+        """
         self.peek.match?(/=/)
     end
 
     def read_string
+        """
+            This function reads a string, starting from a quote character and ending with the same quote character.
+        """
+        # Acquire starting quote
         starting_quote = self.next
         token = ""
         while ! self.end_of_file do
+            # Check if it is a quote character
             if self.is_quotes == false
+                # If it is not a quote character, add it to the token string
                 token = token + self.next
             else
+                # Check if the quote is equal to the starting quote
                 if self.peek == starting_quote
                     break
                 else
@@ -125,11 +162,15 @@ class MacroParser
                 end
             end
         end
+        # Advance to the next position, as the loop broke at the quote position
         self.next
         token
     end
 
     def read_token(skip_spaces = true)
+        """
+        This function reads a token.
+        """
         token = ""
         while ! self.end_of_file do
             if self.is_alphanumeric == true
@@ -146,10 +187,14 @@ class MacroParser
     end
 
     def is_assignment_token
+        """
+            This function returns true if the current token is an assignment.
+        """
         current_reading_position = @reading_position
         parsed_space = false
         result = false
         while ! self.end_of_file do
+            # Checks if the character is alphanumeric and spaces have not already been parsed
             if self.is_alphanumeric == true && parsed_space == false
                 self.next
             elsif self.is_whitespace == true
@@ -159,6 +204,7 @@ class MacroParser
                 break
             end
         end
+        # If the character is an assignment, then the token is an assignment token
         if ! self.end_of_file && self.is_assignment
             result = true
         end
@@ -167,8 +213,13 @@ class MacroParser
     end
 
     def read_assignment
+        """
+            This function reads an assignment and returns an array containing the variable name in the first position, and value in the second.
+        """
         variable_name = ""
         variable_value = ""
+        # read the variable name
+        # TO-DO: use function read token
         while ! self.end_of_file do
             if self.is_alphanumeric == true
                 variable_name = variable_name + self.next
@@ -181,6 +232,7 @@ class MacroParser
             end
         end
         self.next # skip the equal sign
+        # TO-DO use a for loop, this will skip only one space
         if self.is_whitespace == true
             self.next
         elsif is_alphanumeric == true
